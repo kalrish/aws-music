4a7090b3aa7ba794d5abb4da2d898721aeef8fe2
Kalrish Bäakjen <david@davidjsp.eu>
2018-06-16 02:34:32 +0200

    UPDATE

--- ec2/volume/provision.sh
+++ ec2/volume/provision.sh
@@ -6,5 +6,5 @@ set -e
 sudo mke2fs -t ext4 /dev/xvdf
 sudo mkdir /mnt/music
 sudo mount /dev/xvdf /mnt/music
-sudo aws s3 cp --recursive s3://music-sourcebucket-1si6irtpk3shn/schtiel-scans /mnt/music
+sudo aws --region eu-central-1 s3 cp --recursive s3://music-sourcebucket-1si6irtpk3shn/schtiel-scans /mnt/music
 sudo umount /mnt/music
