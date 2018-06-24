3e83cb6b8380908c3d0c2e799c811144576ffdd5
Kalrish Bäakjen <david@davidjsp.eu>
2018-06-25 00:46:38 +0200

    Update

--- ec2/ebs-prepare-sources.sh
+++ ec2/ebs-prepare-sources.sh
@@ -1,4 +1,4 @@
 mke2fs -v -t ext4 -I 128 -b 4096 -m 0 -E lazy_itable_init=0,lazy_journal_init=0 -- /dev/xvdf
-mount /dev/xvdf /mnt
+mount -- /dev/xvdf /mnt
 aws s3 cp --recursive "s3://$1" /mnt
-umount /dev/xvdf
+umount -- /dev/xvdf
