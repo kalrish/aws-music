34851f24e49eaf4c322a12cae257b6d3aa041126
Kalrish Bäakjen <david@davidjsp.eu>
2018-07-01 23:25:59 +0200

    Update

--- ec2/ebs-prepare-builds.sh
+++ ec2/ebs-prepare-builds.sh
@@ -1,6 +1,5 @@
-mke2fs -v -t ext4 -I 128 -b 4096 -m 0 -E lazy_itable_init=0,lazy_journal_init=0 -- /dev/xvdf
-mount -- /dev/xvdf /mnt
-cd /mnt
-mkdir {.overlayfs,builds}
-cd builds
-umount -- /dev/xvdf
+declare -r VOLUME_NAME="$1" MOUNT_POINT='/mnt'
+mke2fs -v -t ext4 -I 128 -b 4096 -m 0 -E lazy_itable_init=0,lazy_journal_init=0 -- "${VOLUME_NAME}"
+mount -- "${VOLUME_NAME}" "${MOUNT_POINT}"
+mkdir "${MOUNT_POINT}"/{.overlayfs,builds}
+umount -- "${VOLUME_NAME}"
--- ec2/ebs-prepare-sources.sh
+++
@@ -1,5 +0,0 @@
-declare -r SOURCES_BUCKET="$1" VOLUME_NAME="$2" MOUNT_POINT='/mnt'
-mke2fs -v -t ext4 -I 128 -b 4096 -m 0 -E lazy_itable_init=0,lazy_journal_init=0 -- "${VOLUME_NAME}"
-mount -- "${VOLUME_NAME}" "${MOUNT_POINT}"
-aws s3 cp --recursive --exclude '*' "s3://${SOURCES_BUCKET}" "${MOUNT_POINT}"
-umount -- "${VOLUME_NAME}"
