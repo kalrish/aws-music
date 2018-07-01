42f2ff761c03556305843d1d72a48c974b5c3a13
Kalrish Bäakjen <david@davidjsp.eu>
2018-07-01 22:32:24 +0200

    Update

--- ec2/ebs-prepare-sources.sh
+++ ec2/ebs-prepare-sources.sh
@@ -1,4 +1,5 @@
-mke2fs -v -t ext4 -I 128 -b 4096 -m 0 -E lazy_itable_init=0,lazy_journal_init=0 -- /dev/xvdf
-mount -- /dev/xvdf /mnt
-aws s3 cp --dryrun --recursive "s3://$1" /mnt
-umount -- /dev/xvdf
+declare -r SOURCES_BUCKET="$1" VOLUME_NAME="$2" MOUNT_POINT='/mnt'
+mke2fs -v -t ext4 -I 128 -b 4096 -m 0 -E lazy_itable_init=0,lazy_journal_init=0 -- "${VOLUME_NAME}"
+mount -- "${VOLUME_NAME}" "${MOUNT_POINT}"
+aws s3 cp --recursive --exclude '*' "s3://${SOURCES_BUCKET}" "${MOUNT_POINT}"
+umount -- "${VOLUME_NAME}"
