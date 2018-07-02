501ce78883474e2033ad42cea8666e5e8ea70942
Kalrish Bäakjen <david@davidjsp.eu>
2018-07-02 03:01:28 +0200

    Update

--- ec2/ebs-prepare-builds.sh
+++
@@ -1,5 +0,0 @@
-declare -r VOLUME_NAME="$1" MOUNT_POINT='/mnt'
-mke2fs -v -t ext4 -I 128 -b 4096 -m 0 -E lazy_itable_init=0,lazy_journal_init=0 -- "${VOLUME_NAME}"
-mount -- "${VOLUME_NAME}" "${MOUNT_POINT}"
-mkdir "${MOUNT_POINT}"/{.overlayfs,builds}
-umount -- "${VOLUME_NAME}"
