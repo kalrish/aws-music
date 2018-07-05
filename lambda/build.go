6e318f2e8274036c117b289775660470f062f3ec
kalrish <david@davidjsp.eu>
2018-07-05 16:45:40 +0200

    Update

--- lambda/build.go
+++ lambda/build.go
@@ -2,12 +2,25 @@ package main
 
 import (
     "github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go"
 )
 
-func hello () (string, error) {
+func build () (string, error) {
+	client := ec2.New(session.New())
+	
+	rv, err := client.RunInstances(
+		&ec2.RunInstancesInput{
+			LaunchTemplate: &ec2.LaunchTemplateSpecification{
+				LaunchTemplateName: "vibes-worker",
+			},
+			MinCount: 1,
+			MaxCount: 1,
+		},
+	)
+	
     return "Hello ƛ!", nil
 }
 
 func main () {
-    lambda.Start(hello)
+    lambda.Start(build)
 }
